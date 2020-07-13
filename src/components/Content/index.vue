50c7e6b56236765b3d12e05e5b8e0486f4d080c5
jyingcode <1002693441@qq.com>
2020-07-14 00:33:49 +0800

    redactData

--- src/components/Content/index.vue
+++ src/components/Content/index.vue
@@ -11,7 +11,7 @@
 					placeholder="审批人"
 				></el-input>
 			</el-form-item>
-			<el-form-item label="角色">
+			<el-form-item v-if="show" label="角色">
 				<el-select v-model="search.roleId" placeholder="请选择">
 					<el-option
 						v-for="(item, index) in role"
@@ -41,25 +41,29 @@
 				>
 				</el-date-picker>
 			</el-form-item>
+
 			<el-form-item>
 				<el-button type="primary" @click="onSubmit">
 					<i class="el-icon-search search"></i>
 					查询</el-button
 				>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="onrefresh">
-					<i class="el-icon-refresh refresh"></i>重置</el-button
-				>
-			</el-form-item>
+				<el-button type="primary" @click="onrefresh" class="but">
+					<i class="el-icon-refresh refresh"></i>重置
+				</el-button>
 
-			<el-button v-if="!show" type="primary" @click="onDeleft"
-				>展开<i class="el-icon-arrow-down"></i>
-			</el-button>
-			<el-button v-if="show" type="primary" @click="onshow"
-				>收起 <i class="el-icon-arrow-up"></i
-			></el-button>
+				<span
+					v-if="!show"
+					type="primary"
+					@click="onDeleft"
+					class="but1"
+				>
+					展开<i class="el-icon-arrow-down"></i>
+				</span>
+				<span v-if="show" type="primary" @click="onshow" class="but1">
+					收起 <i class="el-icon-arrow-up"></i></span
+			></el-form-item>
 		</el-form>
+
 		<el-row>
 			<el-button type="primary" @click="changeData(search)"
 				>新增</el-button
@@ -107,33 +111,39 @@
 			</el-form>
 		</el-dialog>
 		<el-table
+			ref="singleTable"
 			:data="tableData"
 			border
 			style="width: 100%"
 			class="stern-table"
 		>
-			<el-table-column fixed prop="date" label="日期" width="150">
+			<el-table-column fixed prop="date" label="更新时间" width="150">
 			</el-table-column>
-			<el-table-column prop="name" label="姓名" width="120">
+
+			<el-table-column prop="loginName" label="登录名" width="120">
 			</el-table-column>
-			<el-table-column prop="province" label="省份" width="120">
+			<el-table-column prop="linkPerson" label="联系人" width="120">
 			</el-table-column>
-			<el-table-column prop="city" label="市区" width="120">
+			<el-table-column prop="roleId" label="角色" width="120">
 			</el-table-column>
-			<el-table-column prop="address" label="地址" width="300">
+			<el-table-column prop="status" label="状态" width="300">
 			</el-table-column>
-			<el-table-column prop="zip" label="邮编" width="120">
-			</el-table-column>
+
 			<el-table-column fixed="right" label="操作" width="100">
-				<el-button
-					@click="handleClick(scope.row)"
-					type="text"
-					size="small"
-					>查看</el-button
-				>
-				<el-button type="text" size="small" @click="changeData(item)"
-					>编辑</el-button
-				>
+				<template slot-scope="scope">
+					<el-button
+						@click="handleClick(scope.$index, tableData)"
+						type="text"
+						size="small"
+						>删除</el-button
+					>
+					<el-button
+						type="text"
+						size="small"
+						@click="changeData(search)"
+						>编辑</el-button
+					>
+				</template>
 			</el-table-column>
 		</el-table>
 	</div>
@@ -184,36 +194,32 @@ export default {
 			tableData: [
 				{
 					date: '2016-05-02',
-					name: '王小虎',
-					province: '上海',
-					city: '普陀区',
-					address: '上海市普陀区金沙江路 1518 弄',
-					zip: 200333,
+					loginName: '王小虎',
+					linkPerson: '上海',
+					roleId: '普陀区',
+					status: '上海市普陀区金沙江路 1518 弄',
 				},
 
 				{
 					date: '2016-05-04',
-					name: '王小虎',
-					province: '上海',
-					city: '普陀区',
-					address: '上海市普陀区金沙江路 1517 弄',
-					zip: 200333,
+					loginName: '王小虎',
+					linkPerson: '上海',
+					roleId: '普陀区',
+					status: '上海市普陀区金沙江路 1517 弄',
 				},
 				{
 					date: '2016-05-01',
-					name: '王小虎',
-					province: '上海',
-					city: '普陀区',
-					address: '上海市普陀区金沙江路 1519 弄',
-					zip: 200333,
+					loginName: '王小虎',
+					linkPerson: '上海',
+					roleId: '普陀区',
+					status: '上海市普陀区金沙江路 1519 弄',
 				},
 				{
 					date: '2016-05-03',
-					name: '王小虎',
-					province: '上海',
-					city: '普陀区',
-					address: '上海市普陀区金沙江路 1516 弄',
-					zip: 200333,
+					loginName: '王小虎',
+					linkPerson: '上海',
+					roleId: '普陀区',
+					status: '上海市普陀区金沙江路 1516 弄',
 				},
 			],
 		}
@@ -238,12 +244,12 @@ export default {
 		onshow() {
 			this.show = false
 		},
-		handleClick(row) {
-			console.log(row)
+		handleClick(index, rows) {
+			rows.splice(index, 1)
 		},
 		changeData(search) {
 			console.log(search)
-			this.dialogTableVisibl = true
+			this.dialogTableVisible = true
 			this.search = JSON.parse(JSON.stringify(search))
 		},
 	},
@@ -269,6 +275,16 @@ export default {
 	.search,
 	.refresh {
 		margin-right: 10px;
+	}
+	.but {
+		width: 97.89x;
+		height: 40px;
+		margin: 0 0 20px 15px;
+	}
+	.but1 {
+		margin-top: 15px;
+		color: #1890ff;
+		margin-left: 15px;
 	}
 }
 </style>
